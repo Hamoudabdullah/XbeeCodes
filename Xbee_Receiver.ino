e7dc5fcff06abbd4c80e067b170681985c1313d6
Hamoudabdullah <hamoud.alansi@gmail.com>
2016-06-09 13:13:32 +0200

    Written codes
    The second Xbee_Receiver.ino is connected to the motor shield

---
+++ Xbee_Receiver.ino
@@ -0,0 +1,100 @@
+/*
+   This project is for wireless communication between two XBee modules.
+   This is the code for the receiving XBee module.
+
+   Authors William, Elvin och Hamoud
+*/
+
+#include <Printers.h>
+#include <XBee.h>
+
+
+XBee xbee1 = XBee();
+
+
+XBeeResponse response = XBeeResponse();
+Rx16Response rx16 = Rx16Response();
+
+int data = 0;
+int i = 0;
+int k = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  xbee1.setSerial(Serial);
+
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  //DIRECTION
+  pinMode(3, OUTPUT);             //IN_1
+  pinMode(11, OUTPUT);            //IN_2
+  //SLEEP MODE
+  pinMode(12, OUTPUT);            //INH_1
+  pinMode(13, OUTPUT);            //INH_2
+
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+
+
+}
+
+void loop()
+{
+
+  xbee1.readPacket(100);
+
+  //-------------------------------------------------------------------------------------------------------------------------------------
+  if (xbee1.getResponse().isAvailable())                                    //Xbee 1
+  {
+
+    if (xbee1.getResponse().getApiId() == RX_16_RESPONSE)
+    {
+      xbee1.getResponse().getRx16Response(rx16);
+      data = rx16.getData(0);
+
+      Serial.print(" XBee1: ");
+      Serial.print( data );
+      Serial.print("\n");
+      delay(1);
+    }
+
+  }
+  if (data == 66) {               // Hissas upp
+                                  //Hissas sakta upp
+    digitalWrite(2, HIGH);
+    analogWrite(3, i);
+    delay(100);
+    if (i != 255) {
+      i = i + 1;
+    }
+  }
+
+  else if (data == 65) {          // Hissas ner
+    //Hissas sakta upp
+    digitalWrite(2, LOW);
+    analogWrite(11, k);
+    delay(100);
+    if (k != 255) {
+      k = k + 1;
+    }
+  } 
+  
+
+ 
+  
+
+  if (data == 0) {
+    digitalWrite(4, HIGH);
+    digitalWrite(2, LOW);
+    analogWrite(3, 0);
+    analogWrite(11, 0);
+    k=0;
+    i=0;
+  }
+  if (data != 0) {
+    digitalWrite(4, LOW);
+  }
+}
+
---
+++ Xbee_Transmitter.ino
@@ -0,0 +1,64 @@
+/*
+ * This project is for wireless communication between two XBee modules. 
+ * This is the code for the Transmitter XBee module. 
+ * 
+ * Authors William, Elvin och Hamoud
+ */
+
+#include <Printers.h>
+#include <XBee.h>
+
+
+XBee xbee = XBee();
+uint8_t payload[2] ;
+int Up = 0;
+int Stop = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+}
+
+void loop()
+{
+  Stop = digitalRead(6);
+  Up = digitalRead(7);
+
+
+  payload[0] = {NULL};
+
+  if (Stop == LOW) {         //Körs då stop inte är tryckt
+    
+    if (Up == HIGH)
+    {                          //Hissas upp
+     digitalWrite(2, HIGH);
+     digitalWrite(3, LOW);
+      payload[0] = {'A'};
+    }
+    if (Up == LOW) {           //Hissas ner
+      digitalWrite(2, LOW);
+      digitalWrite(3, HIGH);
+      payload[0] = {'B'};
+
+    }
+    digitalWrite(4, LOW);
+  }                          // STOP if-sats
+  if (Stop == HIGH) {
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+ digitalWrite(4, HIGH);
+  }
+  Tx16Request tx16 = Tx16Request(0xFFFF, payload, sizeof(payload));
+
+  xbee.send( tx16 );
+  delay(200);
+
+}
